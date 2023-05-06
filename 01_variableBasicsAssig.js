de702421e09b57d416d006412c252750bda60bd9
Switi02 <sweetylulla02@gmail.com>
2023-05-06 09:43:53 +0530

    Primitive datatypes js files created
    Primitive datatypes js files created

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,25 @@
+// Number type variables
+var maths = 95;
+console.log("Mathematics Marks   :" , maths, "               Type :", typeof maths);
+var science = 92;
+console.log("Science Marks       :" , science, "               Type :", typeof science);
+var english = 85;
+console.log("English Marks       :" , english, "               Type :", typeof english);
+
+// String type variables
+var mathRemarks = "Excellent!";
+console.log("Mathematics Remarks :", mathRemarks, "       Type :", typeof mathRemarks);
+var scienceRemarks = "Good! Keep it up.";
+console.log("Science Remarks     :", scienceRemarks, "Type :", typeof scienceRemarks);
+var englishRemarks = "Can do better.";
+console.log("English Remarks     :", englishRemarks, "   Type :", typeof englishRemarks);
+
+// Boolean type variables
+var isActive = true;
+console.log("Is student Active   :", isActive, "             Type :", typeof isActive);
+var isObidient = true;
+console.log("Is student Obidient :", isObidient, "             Type :", typeof isObidient);
+var isFriendly = false;
+console.log("Is student Friendly :", isFriendly, "            Type :", typeof isFriendly);
+
+
---
+++ 02_typeOf.js
@@ -0,0 +1,17 @@
+var bankName;
+console.log("Name of Bank :", bankName, "        DataType :", typeof bankName);
+bankName = 'SBI Bank';
+console.log("Name of Bank :", bankName, "         DataType :", typeof bankName);
+
+var marks = '90%';
+console.log("Marks        :", marks, "              DataType :", typeof marks);
+
+var isWorking;
+console.log("Is Working   :", isWorking, "        DataType :", typeof isWorking);
+isWorking = true;
+console.log("Is Working   :", isWorking, "             DataType :", typeof isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log("Total Count  :", totalCount, "DataType :", typeof totalCount);
+totalCount = 107;
+console.log("Total Count  :", totalCount, "              DataType :", typeof totalCount);
