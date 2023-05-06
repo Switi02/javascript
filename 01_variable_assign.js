702ca59b38f9abc9c40dfd13a15127e40acd68e3
Switi02 <sweetylulla02@gmail.com>
2023-05-06 09:43:17 +0530

    demo js files created
    demo js files created

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+var myFullName;  // Variable Declaration
+console.log(myFullName);  // Undefined
+myFullName = "Switi Lulla"; // Variable Initialization
+console.log(myFullName); 
+myFullName = "Switi Dilip Lulla"; // Variable Updation
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "First Name :";
+var lastName = "Last Name  :";
+var age = "Age        :";
+var address = "Address    :";
+var pincode = "Pincode    :";
+var gender = "Gender     :";
+var isMarried = "Is Married :";
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pincode);
+console.log(gender);
+console.log(isMarried);
---
+++ hello.js
@@ -0,0 +1,22 @@
+
+
+
+
+console.log("Sweety Lulla.. ");
+console.log("At Satguru Niwas, Behind M.S.E.B., Amalner, Dist-Jalgaon, MH");
+
+var firstName = "Sweety";
+console.log(firstName);
+
+var age; // Variable Declaration
+age = "22"; // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder);  //undefined
+holder="My Specs";  // Variable Initialization
+console.log(holder);
+
+holder = "New Specs";  // Variable Update
+console.log(holder);
+
