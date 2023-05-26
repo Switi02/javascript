b639d6b9ab3769c2238dc9c175699639613d818a
Switi02 <sweetylulla02@gmail.com>
2023-05-26 19:02:21 +0530

    while loop files created
    while loop files created

---
+++ 08_whlie_Assignment.js
@@ -0,0 +1,72 @@
+
+
+
+console.log(`1. WAP to print numbers from 5 to 15 by incrementing 1`);
+let a = 5;
+var str = "Numbers from 5 to 15 :";
+while (a <= 15) {
+    str = str + "  " + a ;
+    a++;
+}
+console.log(str);
+console.log(``);
+
+console.log(`2. WAP to print numbers from 50 to 40 by decrementing by 1`);
+let b = 50;
+var str = "Numbers from 50 to 40 :";
+while (b >= 40) {
+    str = str + "  " + b ;
+    b--;
+}
+console.log(str);
+console.log(``);
+
+console.log(`3. WAP to find first 15 odd numbers`);
+let c = 1;
+var str = "First 15 Odd Numbers :";
+while (c < 30) {
+    str = str + "  " + c ;
+    c = c + 2;
+}
+console.log(str);
+console.log(``);
+
+console.log(`4. WAP to find first 10 even numbers`);
+let d = 0;
+var str = "First 10 Even Numbers :";
+while (d < 20) {
+    str = str + "  " + d ;
+    d = d + 2;
+}
+console.log(str);
+console.log(``);
+
+console.log(`5. WAP to print table of 5 like -> 5 10 15 20 25 ........ 50`);
+let e = 5;
+var str = "Table of 5 :";
+while (e <= 50) {
+    str = str + "  " + e;
+    e = e + 5;
+}
+console.log(str);
+console.log(``);
+
+console.log(`6. WAP to print table of 10 like 10 20 30 40 ........ 100`);
+let f = 10;
+var str = "Table of 10 :";
+while (f <= 100) {
+    str = str + "  " + f;
+    f = f + 10;
+}
+console.log(str);
+console.log(``);
+
+console.log(`7. WAP to print table of 10 in reverse order like -> 100 90 80 70 ....... 10`);
+let g = 100;
+var str = "Table of 10 in reverse order :";
+while (g >= 10) {
+    str = str + "  " + g;
+    g = g - 10;
+}
+console.log(str);
+console.log(``);
